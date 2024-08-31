31a8c4b980319646c32116a8286cae5ac29c6976
Lyxot <1169942595@qq.com>
2024-08-31 21:02:44 +0800

    Init Actions

---
+++ run.py
@@ -0,0 +1,46 @@
+import subprocess
+import requests
+import toml
+import os
+import sys
+
+url = os.getenv("ONEBOT_URL")
+qqid=int(os.getenv("QQID"))
+client_type = os.getenv("CLIENT_TYPE")
+flag_send = bool(os.getenv("SEND_MSG"))
+
+def send_msg(id,msg):
+    data = {
+        "message_type": "private",
+        "user_id": id,
+        "message": msg
+    }
+    response = requests.post(url+'/send_msg', json=data)
+    return response.json()
+
+if __name__ == '__main__':
+    config = toml.load('daily.toml')
+    for i in config['tasks']:
+        if 'params' in i:
+            if 'client_type' in i['params']:
+                i['params']['client_type'] = client_type
+    with open('~/.config/maa/tasks/daily.toml', 'w') as f:
+        toml.dump(config, f)
+
+    process = subprocess.Popen(sys.argv[1:], shell=True, stdout=subprocess.PIPE)
+    output, error = process.communicate()
+
+    msg = ""
+    flag_summary = False
+    for line in output.splitlines():
+        line = line.decode('utf-8')
+        if "Summary" in line:
+            flag_summary = True
+        if flag_summary:
+            msg += line + "\n"
+        print(line)
+
+    process.kill()
+
+    if flag_send:
+        send_msg(qqid, msg)
