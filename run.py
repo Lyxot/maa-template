3a52586c86bfad913b0679a99b9827ad2d71a784
Lyxot <1169942595@qq.com>
2024-09-01 02:46:39 +0800

    seperate run and msg

--- run.py
+++ run.py
@@ -1,57 +1,43 @@
 import subprocess
-import requests
 import toml
 import os
 import pathlib
 
-url = os.getenv("ONEBOT_URL")
-qqid=int(os.getenv("QQID"))
 client_type = os.getenv("CLIENT_TYPE")
-flag_send = bool(os.getenv("SEND_MSG"))
 
-def send_msg(id,msg):
-    data = {
-        "message_type": "private",
-        "user_id": id,
-        "message": msg
-    }
-    response = requests.post(url+'/send_msg', json=data)
-    return response.json()
+config = toml.load('daily.toml')
+for i in config['tasks']:
+    if 'params' in i:
+        if 'client_type' in i['params']:
+            i['params']['client_type'] = client_type
+with open(str(pathlib.Path.home())+'/.config/maa/tasks/daily.toml', 'w') as f:
+    toml.dump(config, f)
 
-if __name__ == '__main__':
-    config = toml.load('daily.toml')
-    for i in config['tasks']:
-        if 'params' in i:
-            if 'client_type' in i['params']:
-                i['params']['client_type'] = client_type
-    with open(str(pathlib.Path.home())+'/.config/maa/tasks/daily.toml', 'w') as f:
-        toml.dump(config, f)
+process = subprocess.Popen("maa run daily", shell=True, stdout=subprocess.PIPE)
+output, error = process.communicate()
 
-    process = subprocess.Popen("maa run daily", shell=True, stdout=subprocess.PIPE)
-    output, error = process.communicate()
+log = ""
+summary = ""
+flag_summary = False
+for line in output.splitlines():
+    line = line.decode('utf-8')
+    if flag_summary:
+        summary += line + "\n"
+    if "Summary" in line:
+        flag_summary = True
+    print(line)
+    log += line + "\n"
+process.kill()
 
-    log = ""
-    summary = ""
-    flag_summary = False
-    for line in output.splitlines():
-        line = line.decode('utf-8')
-        if flag_summary:
-            summary += line + "\n"
-        if "Summary" in line:
-            flag_summary = True
-        print(line)
-        log += line + "\n"
-    process.kill()
+summary_md = "# Summary\n" + summary
+summary_md += "\n\n# Log\n```\n" + log + "```\n"
+os.system('echo "' + summary_md + '" > "$GITHUB_STEP_SUMMARY"')
 
-    summary_md = "# Summary\n" + summary
-    summary_md += "\n\n# Log\n```\n" + log + "```\n"
-    os.system('echo "' + summary_md + '" > "$GITHUB_STEP_SUMMARY"')
-
-    if flag_send:
-        summary_list = summary.splitlines()
-        summary_msg = ""
-        for i in range(len(summary_list)):
-            line = summary_list[i]
-            if line.count('-') > len(line)*0.75:
-                summary_msg += summary_list[i+1] + "\n"
-        send_msg(qqid, summary_msg)
+summary_list = summary.splitlines()
+summary_msg = ""
+for i in range(len(summary_list)):
+    line = summary_list[i]
+    if line.count('-') > len(line)*0.75:
+        summary_msg += summary_list[i+1] + "\n"
+with open('msg', 'w') as f:
+    f.write(summary_msg)
